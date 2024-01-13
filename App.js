a2a9e62c1c59d56444b2eb88aa9686817d12b05f
yendefrr <github@yendefr.xyz>
2024-01-14 00:37:35 +0300

    Initial: Capybaras infitiy list

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Image, FlatList, RefreshControl, StyleSheet } from "react-native";
+
+const baseApiURL = "https://api.tinyfox.dev";
 
 export default function App() {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImage = async () => {
+    try {
+      const response = await fetch(`${baseApiURL}/img.json?animal=capy`);
+      const result = await response.json();
+
+      if (result && result.loc) {
+        const imageUrl = `${baseApiURL}${result.loc}`;
+        return imageUrl;
+      } else {
+        console.error("Unexpected API response:", result);
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching image:", error);
+      return null;
+    }
+  };
+
+  const fetchImages = async () => {
+    const newImages = [];
+
+    while (newImages.length < 5) {
+      const imageUrl = await fetchImage();
+      if (imageUrl) {
+        newImages.push(imageUrl);
+      }
+    }
+
+    setSelectedImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  const renderItem = ({ item }) => <Image style={styles.imageContainer} source={{ uri: item }} />;
+
+  const keyExtractor = (item, index) => index.toString();
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    setSelectedImages([]);
+    await fetchImages();
+    setRefreshing(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={selectedImages}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
+        onEndReached={fetchImages}
+        onEndReachedThreshold={100}
+      />
     </View>
   );
 }
@@ -13,8 +71,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2D2313",
+    paddingTop: 50,
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 340,
+    height: 440,
+    marginBottom: 20,
+    borderRadius: 20,
   },
 });
